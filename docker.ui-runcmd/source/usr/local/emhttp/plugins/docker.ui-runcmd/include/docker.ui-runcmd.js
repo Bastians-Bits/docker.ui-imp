8f5e74773b1536e2d3da7877edf376b2080b9fad
bastiansbits <sebastian.loesch@icloud.com>
2023-06-03 22:51:43 +0200

    Added method to open a modal dialog

--- docker.ui-runcmd/source/usr/local/emhttp/plugins/docker.ui-runcmd/include/docker.ui-runcmd.js
+++ docker.ui-runcmd/source/usr/local/emhttp/plugins/docker.ui-runcmd/include/docker.ui-runcmd.js
@@ -4,3 +4,7 @@ $('<input/>', {
     value: 'Show Run Command',
     style: 'display:none'
 }).insertAfter('div.content > input:last');
+
+function openShowRunCommandDialog() {
+    alert("Works!");
+}
